98ef651ffa35b3b49a0024a763339f38e1d2d4ef
leva <levakhnazarov@gmail.com>
2020-04-25 00:31:02 +0800

    [CONF] moved pwd to env

--- src/api/exchanges.ts
+++ src/api/exchanges.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const getExchanges = (params: any) => {
   return axios({
     headers: {
-      'Authorization': 'Nx6\\0v%N++\\Oq&mNRwJ7iomOlzoYn'
+      'Authorization': process.env.APOLLO_AUTHORIZATION_HEADER
     },
     url: 'https://apollo.atomicwallet.io/transactions/all',
     method: 'get',
