f2f76dbace20b6c833254a19d5d40d95dfff6dae
Muxa97 <mishamalygin.97@yandex.ru>
2020-09-24 17:55:25 +0300

    Added staking stats

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -176,7 +176,9 @@ export default {
     currency: 'Currency',
     yearlyIncome: 'Yearly Income',
     platform: 'Platform',
-    walletVersion: 'Wallet Version'
+    walletVersion: 'Wallet Version',
+    stakedUsd: 'Staked USD',
+    staked: 'Staked'
   },
   example: {
     warning: 'Creating and editing pages cannot be cached by keep-alive because keep-alive\'s \'include\' currently does not support caching based on routes, so now it\'s cached based on component name. If you want to achieve a similar caching effect, you can use a browser caching scheme such as localStorage. Or don\'t use keep-alive\'s \'include\' to cache all pages directly. See details'
